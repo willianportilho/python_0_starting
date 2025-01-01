c83765368efd78bd3f1cc8fe0ab337073ca72afa
Willian <willianportilho01@gmail.com>
2024-12-31 23:58:09 -0500

    feat: adds ex03 dir; adds NULL_not_found.py and tester.py functions.

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,19 @@
+def NULL_not_found(object: any) -> int:
+    types = [None, 0, False]
+    types_str = ["Nothing", "Zero", "Fake"]
+
+    for idx, tp in enumerate(types):
+        if (type(object) == type(tp)):
+            print(types_str[idx] + ":", object, type(object))
+            return(0)
+    
+    if (object == ''):
+        print("Empty:" + object, type(object))
+        return(0)
+    elif (object != object):
+        print("Cheese:", object, type(object))
+        return(0)
+    else:
+        print("Type not Found")
+
+    return(1)
