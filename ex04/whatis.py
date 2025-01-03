18bfde65bc38a56d8ebaf5a7df2db9735ce7a0b6
Willian <willianportilho01@gmail.com>
2025-01-02 19:30:42 -0500

    feat: adds '+' and '-' signs protection

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -6,6 +6,12 @@ try:
 
     assert len(sys.argv[1]) != 0, "AssertError: no valid input provided, argument is empty"
 
+    user_imput = sys.argv[1]
+    if user_imput[0] == '-' or user_imput[0] == '+':
+        user_imput = user_imput[1:]
+    if not user_imput.isdigit():
+        raise AssertionError("AssertionError: argument is not an integer")
+
     number = int(sys.argv[1])
     assert isinstance(number, int), "AssertionError: argument is not an integer"
 
@@ -15,5 +21,3 @@ try:
         print("I'm Odd.")
 except AssertionError as e:
     print(e)
-except ValueError as e:
-    print("AssertionError: argument is not an integer")
