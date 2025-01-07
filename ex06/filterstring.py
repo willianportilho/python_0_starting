87cb173034993c3ced7d6443bd9d966ea1523328
Willian <willianportilho01@gmail.com>
2025-01-07 18:39:57 -0300

    feat: adds __doc__ in the functions; norminette ok

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -2,12 +2,25 @@ import sys
 
 
 def check_arguments(args):
+    """Validates the arguments passed to the script.
+
+This function checks if the arguments passed to the script are:
+    - Exactly two arguments.
+    - The first argument (text) contains only \
+alphanumeric characters and spaces.
+    - The second argument (number) is a valid integer (positive or negative).
+
+Args:
+    args (list): The list of arguments passed to the script.
+
+Raises:
+    AssertionError: If any of the validation checks fail.
+    """
 
     if len(sys.argv) != 3:
         raise AssertionError(
             "AssertionError: the arguments are bad")
 
-    
     text = args[0]
     if any((not char.isalnum()) and (not char.isspace())
            for char in text):
@@ -27,12 +40,40 @@ def check_arguments(args):
 
 
 def handle_text(text: str, number: int):
+    """Filters words from the text that have more \
+than a specified number of characters.
+
+This function splits the input text into words \
+and returns a list of words whose \
+length is greater than the specified number.
+
+Args:
+    text (str): The input text to be filtered.
+    number (int): The minimum length of the words to keep.
+
+Returns:
+    list: A list of words with a length greater than the specified number.
+    """
+
     word_list: list = text.split(" ")
 
-    return [word for word in word_list if (lambda word: len(word) > number)(word)]
+    return [word for word in word_list
+            if (lambda word: len(word) > number)(word)]
 
 
 def main():
+    """Main entry point of the script.
+
+This function:
+    - Checks if the arguments passed to the script \
+are valid using `check_arguments`.
+    - Calls `handle_text` to filter words from the input text \
+that are longer than the specified length.
+    - Prints the resulting filtered list of words.
+
+Returns:
+    None
+    """
     try:
         check_arguments(sys.argv[1:])
     except AssertionError as e:
