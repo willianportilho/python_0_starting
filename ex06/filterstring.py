805e4f0d617660ce8e5edf69a35e2cd3bae98982
Willian <willianportilho01@gmail.com>
2025-01-07 17:47:13 -0300

    feat: adds filterstring.py with check_arguments function.

---
+++ ex06/filterstring.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def check_arguments(args):
+
+    if len(sys.argv) != 3:
+        raise AssertionError(
+            "AssertionError: the arguments are bad")
+
+    
+    text = args[0]
+    if any((not char.isalnum()) and (not char.isspace())
+           for char in text):
+        raise AssertionError(
+            "AssertionError: the arguments are bad")
+
+    number = args[1]
+    try:
+        if (number[0] == '-') or (number[0] == '+'):
+            number = number[1:]
+        if not number.isdigit():
+            raise AssertionError(
+                "AssertionError: the arguments are bad")
+    except ValueError:
+        raise AssertionError(
+            "AssertionError: the arguments are bad")
+
+
+def main():
+    try:
+        check_arguments(sys.argv[1:])
+    except AssertionError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
