a0f0d2189179a45cfa680c960dc926a5f819458c
Willian <willianportilho01@gmail.com>
2025-01-07 20:15:45 -0300

    fix: now, when there are no arguments to check, the program prints nothing.

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,28 +1,27 @@
 import sys
 
 try:
-    assert len(sys.argv) > 1, ""
+    if len(sys.argv) != 1:
+        assert len(sys.argv) < 3, \
+            "AssertionError: more than one argument is provided"
 
-    assert len(sys.argv) < 3, \
-        "AssertionError: more than one argument is provided"
+        assert len(sys.argv[1]) != 0, \
+            "AssertError: no valid input provided, argument is empty"
 
-    assert len(sys.argv[1]) != 0, \
-        "AssertError: no valid input provided, argument is empty"
+        user_input = sys.argv[1]
+        if (user_input[0] == '-') or (user_input[0] == '+'):
+            user_input = user_input[1:]
+        if not user_input.isdigit():
+            raise AssertionError(
+                "AssertionError: argument is not an integer")
 
-    user_input = sys.argv[1]
-    if (user_input[0] == '-') or (user_input[0] == '+'):
-        user_input = user_input[1:]
-    if not user_input.isdigit():
-        raise AssertionError(
-            "AssertionError: argument is not an integer")
+        number = int(sys.argv[1])
+        assert isinstance(number, int), \
+            "AssertionError: argument is not an integer"
 
-    number = int(sys.argv[1])
-    assert isinstance(number, int), \
-        "AssertionError: argument is not an integer"
-
-    if number % 2 == 0:
-        print("I'm Even.")
-    else:
-        print("I'm Odd.")
+        if number % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
 except AssertionError as e:
     print(e)
