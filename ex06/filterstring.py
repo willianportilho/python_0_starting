db5619162214aa8740a817ce9547839e204ecdd5
Willian <willianportilho01@gmail.com>
2025-01-07 18:25:01 -0300

    feat: ex06 complete (__doc__ in progress)

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -26,11 +26,19 @@ def check_arguments(args):
             "AssertionError: the arguments are bad")
 
 
+def handle_text(text: str, number: int):
+    word_list: list = text.split(" ")
+
+    return [word for word in word_list if (lambda word: len(word) > number)(word)]
+
+
 def main():
     try:
         check_arguments(sys.argv[1:])
     except AssertionError as e:
         print(e)
+    else:
+        print(handle_text(sys.argv[1], int(sys.argv[2])))
 
 
 if __name__ == "__main__":
